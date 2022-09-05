2a137111be82ce90a3752e57272797aaf6280716
Jonathan Steele <jsteele@stripe.com>
2022-09-05 14:51:45 +0100

    feat: Tidy the product listing UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,5 +3,9 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="max-w-xl mx-auto py-12 divide-y md:max-w-4xl">
+      <Component {...pageProps} />
+    </div>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { GetStaticProps, NextPage } from 'next'
-import React from 'react'
+
+import * as React from 'react'
 import Stripe from 'stripe'
 
 type NextPageWithProducts<P = {}, IP = P> = NextPage<P, IP> & {
@@ -47,16 +48,50 @@ export default function Index({ products }: NextPageWithProducts) {
 
   return (
     <React.Fragment>
-      <input
-        value={customerEmail}
-        onChange={(e) => setCustomerEmail(e.target.value)}
-        type="email"
-      />
-      {products.map((product) => {
-        const price: string = (product.default_price as Stripe.Price).id
+      <label className="block">
+        <span className="text-gray-700">Customer email</span>
+        <input
+          value={customerEmail}
+          onChange={(e) => setCustomerEmail(e.target.value)}
+          type="email"
+          className="mt-1 block w-full"
+        />
+      </label>
+      <div className="mx-auto mt-8 max-w-7xl px-4 sm:px-6 lg:px-8">
+        <ul
+          role="list"
+          className="grid gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8"
+        >
+          {products.map((product) => {
+            const price = product.default_price as Stripe.Price
 
-        return <button onClick={() => priceCheckout({ price })}>Buy</button>
-      })}
+            return price ? (
+              <li key={product.id} className="relative">
+                <div className="aspect-w-10 aspect-h-7 group block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                  <img
+                    src={product.images[0]}
+                    className="pointer-events-none object-cover group-hover:opacity-75"
+                  />
+                </div>
+                <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
+                  {product.name}
+                </p>
+                <p className="pointer-events-none block text-sm font-medium text-gray-500">
+                  {new Intl.NumberFormat('en-GB', {
+                    style: 'currency',
+                    currency: price.currency,
+                    maximumFractionDigits: 0,
+                    minimumFractionDigits: 0
+                  }).format(price.unit_amount / 100)}
+                </p>
+                <button onClick={() => priceCheckout({ price: price.id })}>
+                  Buy now
+                </button>
+              </li>
+            ) : null
+          })}
+        </ul>
+      </div>
     </React.Fragment>
   )
 }
